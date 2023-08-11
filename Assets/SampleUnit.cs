cd3f5ae650d0e1fb4174de2eae1afcafac76f436
rkmoon <rkmoon03@gmail.com>
2023-08-11 18:29:47 -0500

    yes

---
+++ Assets/SampleUnit.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TbsFramework.Units;
+
+public class SampleUnit : Unit
+{
+    public Color LeadingColor;
+    public override void Initialize()
+    {
+        base.Initialize();
+        GetComponentInChildren<Renderer>().material.color = LeadingColor;
+    }
+
+    public override void MarkAsFinished()
+    {
+        GetComponentInChildren<Renderer>().material.color = Color.gray;
+    }
+
+    public override void MarkAsFriendly()
+    {
+        GetComponentInChildren<Renderer>().material.color = new Color(0.8f, 1, 0.8f);
+    }
+
+    public override void MarkAsReachableEnemy()
+    {
+        GetComponentInChildren<Renderer>().material.color = Color.red;
+    }
+
+    public override void MarkAsSelected()
+    {
+        GetComponentInChildren<Renderer>().material.color = Color.green;
+    }
+
+    public override void UnMark()
+    {
+        GetComponentInChildren<Renderer>().material.color = LeadingColor;
+    }
+}
